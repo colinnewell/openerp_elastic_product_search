eb5d452c76f578272a185310c7976d5c2222bbf6
Colin Newell <colin.newell@gmail.com>
2012-08-18 17:08:49 +0100

    Started to filter the values we want

--- openerp_elastic_product_search/product_search.py
+++ openerp_elastic_product_search/product_search.py
@@ -7,21 +7,35 @@ from pyes import ES
 # how do I deal with the connection settings?
 conn = ES('127.0.0.1:9200')
 
+
 class product_search(osv.osv):
     _name = "product.product"
     _inherit = "product.product"
 
+    # FIXME: ought to figure out what fields we care about.
+    _columns_to_search = ['description', 'description_sale',
+                            'name']
+
     def write(self, cr, user, ids, vals, context=None):
         import pdb; pdb.set_trace()
-        #conn.update(vals, "openerp", "product", ids)
-        super(product_search, self).write(cr, user, ids, vals, context)
+        data = self._filter_values(vals)
+        if len(data) > 0:
+            #conn.update(vals, "openerp", "product", ids)
+            pass
+        return super(product_search, self).write(cr, user, ids, vals, context)
 
     def create(self, cr, user, vals, context=None):
         import pdb; pdb.set_trace()
-        #conn.index(vals, "openerp", "product", ids)
-        super(product_search, self).create(cr, user, vals, context)
+        o = super(product_search, self).create(cr, user, vals, context)
+        # o is the id
+        # vals is the data structure
+        data = self._filter_values(vals)
+        conn.index(data, "openerp", "product", o)
+        return o
+
+    def _filter_values(self, vals):
+        return dict([v for v in vals.items()
+                            if v[0] in self._columns_to_search])
 
 
 product_search()
-
-
