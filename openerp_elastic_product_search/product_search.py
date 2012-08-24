13bd9bca602bd74764e151755a0e43766c7b0e2e
Colin Newell <colin.newell@gmail.com>
2012-08-24 20:34:54 +0100

    Refactored the index name creation out to a method

--- openerp_elastic_product_search/product_search.py
+++ openerp_elastic_product_search/product_search.py
@@ -40,7 +40,7 @@ class product_search(osv.osv):
             if len(data) > 0:
                 if isinstance(ids, (int, long)):
                     ids = [ids]
-                index = "openerp_" + cr.dbname
+                index = self._index_name(cr)
                 for prod_id in ids:
                     try:
                         conn.update(data, index, "product", prod_id)
@@ -64,8 +64,8 @@ class product_search(osv.osv):
         o = super(product_search, self).create(cr, user, vals, context)
         if o:
             data = self._filter_values(vals)
-            # FIXME how safe is the dbname for this purpose?
-            conn.index(data, "openerp_" + cr.dbname, "product", o)
+            index = self._index_name(cr)
+            conn.index(data, index, "product", o)
         return o
 
     def unlink(self, cr, uid, ids, context=None):
@@ -73,8 +73,9 @@ class product_search(osv.osv):
         if success and ids:
             if isinstance(ids, (int, long)):
                 ids = [ids]
+            index = self._index_name(cr)
             for prod_id in ids:
-                conn.delete("openerp_" + cr.dbname, "product", prod_id)
+                conn.delete(index, "product", prod_id)
         return success
 
     def _filter_values(self, vals):
@@ -83,6 +84,11 @@ class product_search(osv.osv):
         return dict([v for v in vals.items()
                             if v[0] in self._columns_to_search])
 
+    def _index_name(self, cr):
+        # FIXME how safe is the dbname for this purpose?
+        index = "openerp_" + cr.dbname
+        return index
+
     def reindex(self, cr, uid):
         prod_ids = self.search(cr, uid, [])
         # FIXME: is reading all the products in one go a good idea?
@@ -90,7 +96,7 @@ class product_search(osv.osv):
         prods = self.read(cr, uid, prod_ids)
         # FIXME: also think about making use of the elastic search
         # bulk operations
-        index = "openerp_" + cr.dbname
+        index = self._index_name(cr)
         for product in prods:
             data = self._filter_values(product)
             _logger.debug("%s: Indexing product %d" % (index, product['id']))
