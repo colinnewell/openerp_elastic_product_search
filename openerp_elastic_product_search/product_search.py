d4aaf48184141e57dca6f633c3c2ecd55cd0a1fe
Colin Newell <colin.newell@gmail.com>
2012-09-17 20:07:28 +0100

    Started to refactor (doesn't work)

--- openerp_elastic_product_search/product_search.py
+++ openerp_elastic_product_search/product_search.py
@@ -14,20 +14,14 @@ _logger.info('Connecting to ElasticSearch server %s' % server)
 
 # TODO: at some point a lot of this code could probably be
 # refactored out into a trait.
-# FIXME: it would be ideal if we could configure the 
+# FIXME: it would be ideal if we could configure the
 # classes and fields we're interested in.
 
 
-class product_search(osv.osv):
-    _name = "product.product"
-    _inherit = "product.product"
-
-    # FIXME: ought to figure out what fields we care about.
-    _columns_to_search = ['description', 'description_sale',
-                            'name']
+class search_mixin(object):
 
     def write(self, cr, user, ids, vals, context=None):
-        success = super(product_search, self).write(cr, user, ids,
+        success = super(search_mixin, self).write(cr, user, ids,
                                                     vals, context)
         if success and ids:
             data = self._filter_values(vals)
@@ -37,7 +31,7 @@ class product_search(osv.osv):
                 index = self._index_name(cr)
                 for prod_id in ids:
                     try:
-                        conn.update(data, index, "product", prod_id)
+                        conn.update(data, index, search_table, prod_id)
                     except pyes.exceptions.IndexMissingException:
                         # may as well just index what we have.
                         # NOTE: this isn't the only time we might create
@@ -45,37 +39,37 @@ class product_search(osv.osv):
                         # notice we are.
                         _logger.info('Creating index %s in ElasticSearch'
                                         % index)
-                        conn.index(data, index, "product", prod_id)
+                        conn.index(data, index, search_table, prod_id)
                     except pyes.exceptions.NotFoundException:
                         # may as well just index what we have.
-                        _logger.debug('Product %d not found in index'
-                                        % prod_id)
-                        conn.index(data, index, "product", prod_id)
+                        _logger.debug('%s %d not found in index'
+                                        % (search_table, prod_id))
+                        conn.index(data, index, search_table, prod_id)
 
         return success
 
     def create(self, cr, user, vals, context=None):
-        o = super(product_search, self).create(cr, user, vals, context)
+        o = super(search_mixin, self).create(cr, user, vals, context)
         if o:
             data = self._filter_values(vals)
             if len(data) > 0:
                 index = self._index_name(cr)
-                conn.index(data, index, "product", o)
+                conn.index(data, index, search_table, o)
         return o
 
     def unlink(self, cr, uid, ids, context=None):
-        success = super(product_search, self).unlink(cr, uid, ids, context)
+        success = super(search_mixin, self).unlink(cr, uid, ids, context)
         if success and ids:
             if isinstance(ids, (int, long)):
                 ids = [ids]
             index = self._index_name(cr)
             for prod_id in ids:
-                conn.delete(index, "product", prod_id)
+                conn.delete(index, search_table, prod_id)
         return success
 
     def _filter_values(self, vals):
         return dict([v for v in vals.items()
-                            if v[0] in self._columns_to_search])
+                            if v[0] in self.columns_to_search])
 
     def _index_name(self, cr):
         # FIXME how safe is the dbname for this purpose?
@@ -93,9 +87,30 @@ class product_search(osv.osv):
         for product in prods:
             data = self._filter_values(product)
             if len(data) > 0:
-                _logger.debug("%s: Indexing product %d" % (index, product['id']))
+                _logger.debug("%s: Indexing %s %d" %
+                            (index, search_table, product['id']))
                 _logger.debug(data)
-                conn.index(data, index, "product", product['id'])
+                conn.index(data, index, search_table, product['id'])
 
 
+class product_template_search(osv.osv, search_mixin):
+    _name = "product.template"
+    _inherit = "product.template"
+
+    columns_to_search = ['description', 'description_sale',
+                            'name']
+    search_table = 'product_template'
+
+
+class product_search(osv.osv, search_mixin):
+    _name = "product.product"
+    _inherit = "product.product"
+
+    # FIXME: ought to figure out what fields we care about.
+    columns_to_search = ['description', 'description_sale',
+                            'name']
+    search_table = 'product'
+
+
+product_template_search()
 product_search()
